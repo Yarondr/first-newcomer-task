83ec291b71d98a8b64a9a45aa5736ba15de8a8ee
Yarondr <yarondrukh@gmail.com>
2023-05-17 01:53:59 +0300

    Update title, desc and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,16 @@ import { AppProps } from 'next/app'
 import styles from '../styles/page.module.css'
 import '../styles/globals.css'
 import RootLayout from '@/components/layout'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
+      <Head>
+        <title>FLL Scoring System</title>
+        <meta name="description" content="FLL Scoring System - Cargo Connect" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </RootLayout>
   )
